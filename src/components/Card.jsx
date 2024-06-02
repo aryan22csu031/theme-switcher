d567223acc4990f51f095a0027465fff69a61221
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-06-02 18:46:57 +0530

    fixed tailwind config

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Card() {
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-black dark:border-black">
             <a href="/">
                 <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-red-600">
                         Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
                     </h5>
                 </a>
